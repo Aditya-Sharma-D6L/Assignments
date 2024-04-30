0526b3654bdf3a8535f77450182dd713282cb1d6
Aditya-Sharma-D6L <aditya.sharma@delta6labs.com>
2024-04-30 16:21:39 +0530

    added material ui assignments

--- frontend/testing-assignment/src/App.js
+++ frontend/testing-assignment/src/App.js
@@ -1,9 +1,38 @@
-function App() {
+/////////////////////////////////////////////////////
+// import Header from "./components/Header";
+// import LoginForm from "./components/LoginForm";
+
+// function App() {
+//   return (
+//     <div>
+//       <Header title="Welcome to My App" variant="h2" />
+//       <Header title="About Us" variant="h3" />
+//       <Header title="Contact" variant="h4" />
+
+//       <LoginForm />
+//     </div>
+//   );
+// }
+
+// export default App;
+////////////////////////////////////////////////////
+
+import React from "react";
+import UserList from "./components/UserList";
+
+const usersData = [
+  { username: "john_doe", password: "password123" },
+  { username: "jane_smith", password: "securePass456" },
+  { username: "sam_jackson", password: "strongPassword789" },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h2>User List</h2>
+      <UserList users={usersData} />
     </div>
   );
-}
+};
 
 export default App;
